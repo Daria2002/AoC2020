b097d1ad5a38a62234ac5aadea8853e5b34c11c7
Daria2002 <daria.matkovic@fer.hr>
2020-11-30 22:00:05 +0100

    Add initial structure

---
+++ build/CMakeFiles/day01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day01.dir/src/day01.cpp.o"
+  "day01.pdb"
+  "day01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/day01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
