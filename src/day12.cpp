46807f6b14ea51497afde77959a4fd6ecf479990
Daria2002 <daria.matkovic@fer.hr>
2020-12-12 23:06:41 +0100

    Add solution for day12

--- src/day12.cpp
+++ src/day12.cpp
@@ -15,6 +15,12 @@ class Instruction
         int value;
 };
 
+std::ostream& operator<<(std::ostream& os, Instruction i)
+{
+    os << "\ninstruction = " << i.action << i.value << '\n';
+    return os;
+}
+
 std::vector<Instruction> get_instructions(const std::string file_name)
 {
     std::ifstream file(file_name);
@@ -94,39 +100,47 @@ void part1(std::vector<Instruction> instructions)
 
 Position execute_instructions_actual(std::vector<Instruction> instructions)
 {
-    Position waypoint(10, 1);
-    Position ship(10, 1);
+    Position waypoint(1, 10);
+    Position ship(0, 0);
     for(Instruction instruction : instructions)
     {
+        std::cout << instruction;
         if(instruction.action == 'N' || instruction.action == 'S')
         {
-            position.north += (instruction.value * (instruction.action == 'N' ? 1 : -1));
+            waypoint.north += (instruction.value * (instruction.action == 'N' ? 1 : -1));
         }
         else if(instruction.action == 'E' || instruction.action == 'W')
         {
-            position.east += (instruction.value * (instruction.action == 'E' ? 1 : -1));
+            waypoint.east += (instruction.value * (instruction.action == 'E' ? 1 : -1));
         }
         else if(instruction.action == 'F')
         {
-            if(position.direction == 1 || position.direction == 3) position.east += (instruction.value * (position.direction == 1 ? 1 : -1));
-            else if(position.direction == 0 || position.direction == 2) position.north += (instruction.value * (position.direction == 0 ? 1 : -1));
+            int north_delta = instruction.value * waypoint.north;
+            int east_delta = instruction.value * waypoint.east;
+            ship.east += (east_delta * (ship.direction != 3 ? 1 : -1));
+            ship.north += (north_delta * (ship.direction != 2 ? 1 : -1));
+            
         }
         else if(instruction.action == 'R')
         {
-            int turns = instruction.value / 90;
-            position.direction += turns;
-            position.direction %= 4;
+            int north = waypoint.north;
+            int east = waypoint.east;
+            waypoint.north = north * (int) cos(M_PI / 180 * instruction.value) - east * (int) sin(M_PI / 180 * instruction.value);
+            waypoint.east = north * (int) sin(M_PI / 180 * instruction.value) + east * (int) cos(M_PI / 180 * instruction.value);
         }
         else if(instruction.action == 'L')
         {
-            int turns = instruction.value / 90;
-            position.direction -= turns;
-            position.direction %= 4;
-            if(position.direction < 0) position.direction += 4;
+            int north = waypoint.north;
+            int east = waypoint.east;
+            waypoint.north = north * (int) cos(-M_PI / 180 * instruction.value) - east * (int) sin(-M_PI / 180 * instruction.value);
+            waypoint.east = north * (int) sin(-M_PI / 180 * instruction.value) + east * (int) cos(-M_PI / 180 * instruction.value);
         }
-        std::cout << "Position: north = " << position.north << ", east = " << position.east << '\n';
+        std::cout << "Ship: north = " << ship.north << ", east = " << ship.east << '\n';
+        std::cout << "Waypoint: north = " << waypoint.north << ", east = " << waypoint.east << '\n';
+        // std::string empty_str;
+        // std::getline(std::cin, empty_str);
     }
-    return position;
+    return ship;
 }
 
 void part2(std::vector<Instruction> instructions)
