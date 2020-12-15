4b229eb0c0ec16efa92f789c42e484d3a05d3f09
Daria2002 <daria.matkovic@fer.hr>
2020-12-15 11:49:10 +0100

    Add solution for day15

--- src/day15.cpp
+++ src/day15.cpp
@@ -18,21 +18,21 @@
 class MemoryGame
 {
     public:
-        MemoryGame(std::vector<int> start_num) : starting_numbers(start_num) {}
-        std::vector<int> starting_numbers;
-        int play(const int end_turn)
+        MemoryGame(std::vector<long long> start_num) : starting_numbers(start_num) {}
+        std::vector<long long> starting_numbers;
+        long long play(const long long end_turn)
         {
             // key - number, value - vector of indices
-            std::map<int, std::vector<int>> map;
-            int last_spoken;
-            int turn = 1;
+            std::map<long long, std::vector<long long>> map;
+            long long last_spoken;
+            long long turn = 1LL;
             std::transform(starting_numbers.begin(), starting_numbers.end(), std::inserter(map, map.end()),
-            [&](const int num) 
+            [&](const long long num) 
             { 
-                std::vector<int> indices {turn};
+                std::vector<long long> indices {turn};
                 last_spoken = num;
                 turn++;
-                return std::pair<int, std::vector<int>>(num, indices); 
+                return std::pair<long long, std::vector<long long>>(num, indices); 
             });
             while(turn <= end_turn)
             {
@@ -51,32 +51,33 @@ class MemoryGame
         }
 };
 
-void part1(std::vector<int> puzzle_input)
+void part1(MemoryGame game)
 {
     std::cout << "======\nPart 1\n======\n";
-    MemoryGame game(puzzle_input);
-    constexpr int end = 2020;
-    std::cout << "2020th number spoken = " << game.play(end) << '\n';
+    constexpr long long end = 2020LL;
+    std::cout << end << "th number spoken = " << game.play(end) << '\n';
 }
 
-void part2(std::vector<int> puzzle_input)
+void part2(MemoryGame game)
 {
     std::cout << "======\nPart 2\n======\n";
+    constexpr long long end = 30000000LL;
+    std::cout << end <<  "th number spoken = " << game.play(end) << '\n';
 }
 
-std::vector<int> get_input(const std::string file_name)
+std::vector<long long> get_input(const std::string file_name)
 {
     std::ifstream file(file_name);
     std::string line;
     std::vector<std::string> line_elements;
-    std::vector<int> all_elements;
+    std::vector<long long> all_elements;
     if(file.is_open())
     {
         while (std::getline(file, line)) 
         {
             boost::split(line_elements, line, boost::is_any_of(","), boost::token_compress_on);
             std::transform(line_elements.begin(), line_elements.end(), std::back_inserter(all_elements),
-               [](const std::string& str) { return std::stoi(str); });
+               [](const std::string& str) { return std::stoll(str); });
         }
     }
     file.close();
@@ -86,9 +87,10 @@ std::vector<int> get_input(const std::string file_name)
 int main()
 {
     const std::string file_name = "/home/daria/Documents/AoC2020/input/day15.txt";
-    std::vector<int> puzzle_input = get_input(file_name);
-    part1(puzzle_input);
+    std::vector<long long> puzzle_input = get_input(file_name);
+    MemoryGame game(puzzle_input);
+    part1(game);
     std::cout << '\n';
-    part2(puzzle_input);
+    part2(game);
     std::cout << '\n';
 }
